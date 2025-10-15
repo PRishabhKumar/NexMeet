2763915c70a73c94fc8ffb8f110785083638d8b6
P Rishabh Kumar <rishabh260405@gmail.com>
2025-10-15 11:14:53 +0530

    Fixed landing page spacing issue

--- Frontend/src/Pages/Home Page/HomePage.jsx
+++ Frontend/src/Pages/Home Page/HomePage.jsx
@@ -1,11 +1,21 @@
 import "./Styles/HomePageStyles.css";
-import LiquidEther from './LiquidEther';
+import DotGrid from "./DotGrid";
 
 function HomePage() {
     return (
-       <div className="mainContentContainer">
-        <h1>This is the home page</h1>
-       </div>
+        <div className="mainContentContainer">
+            <DotGrid
+                dotSize={10}
+                gap={51}
+                baseColor="#1a1a2e"
+                activeColor="#00fff5"
+                proximity={170}
+                shockRadius={500}
+                shockStrength={5}
+                resistance={750}
+                returnDuration={1.5}
+            />
+        </div>
     );
 }
 
